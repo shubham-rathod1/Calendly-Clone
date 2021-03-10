0baaa5bcd2ba432497ec4b40a8909f9698927145
Ajmal KV <reallyaju@gmail.com>
2021-03-10 20:27:22 +0530

    Shown data using js

--- teams_script.js
+++ teams_script.js
@@ -2,11 +2,13 @@ async function showData(){
     var data = fetch('./integrations_json.json');
     data = await data;
     data = await data.json();
+    var html = "";
     for(key in data.teams){
 
         console.log(data.teams[key]);
-        
+        html += `<div class="datadiv" id="${key}"><img src="${data.teams[key][0]}"/><h1>${data.teams[key][1]}</h1><p>${data.teams[key][2]}</p></div>`
     }
+    document.getElementById('grid_container').innerHTML = html;
 }
 
 showData()
