61fd589f48e8aeaca5868fe2fa9d7c69d9be2cf8
Ajmal KV <reallyaju@gmail.com>
2021-03-10 10:00:01 +0530

    Created scripts

---
+++ integrations_script.js
@@ -0,0 +1,71 @@
+document.getElementById('popular_btn').addEventListener('click',getData)
+document.getElementById('apps_btn').addEventListener('click',getData)
+document.getElementById('calendars_btn').addEventListener('click',getData)
+document.getElementById('crm_btn').addEventListener('click',getData)
+document.getElementById('video_btn').addEventListener('click',getData)
+document.getElementById('market_btn').addEventListener('click',getData)
+document.getElementById('ats_btn').addEventListener('click',getData)
+document.getElementById('payment_btn').addEventListener('click',getData)
+document.getElementById('analyt_btn').addEventListener('click',getData)
+document.getElementById('api_btn').addEventListener('click',getData)
+document.getElementById('others_btn').addEventListener('click',getData)
+
+async function getData()
+{
+    var btn_value = event.target.value;
+    if(btn_value==null){
+        btn_value = "Popular";
+    }
+    var data = fetch('./integrations_json.json')
+    data = await data;
+    data = await data.json();
+    showData(data[btn_value],data.header[btn_value]);
+}
+window.addEventListener('load',getData)
+
+function showData(data,header){
+    document.getElementById('top_img').src = header[0];
+    document.getElementById('top_h1').textContent = header[1];
+    document.getElementById('top_p').textContent = header[2];
+    var html = "";
+    len = data.length;
+    for(let i=0;i<data.length;i++)
+    {
+        if(i%2==0)
+        {
+            html += `<tr>`
+        }
+        html +=`<td><div id="data_div${i}"><img src='${data[i].image}'/>
+        <h1><strong>${data[i].title}</h1></strong>
+        <p>${data[i].description}</p><a href="#" id="data_a${i}" class="a_seed">See Details</a></div>
+        </td>`;
+        if(i%2==1)
+        {
+            html += `</tr>`
+        }
+    }
+    document.getElementById('show_data').innerHTML = html;
+}
+var id1;
+var len;
+window.onmouseover = e =>{
+    var id = e.target.id;
+    var idno = (e.target.id).substring(8)
+    if(id.substring(0,8) == 'data_div'){
+        document.getElementById(`data_a${idno}`).style.display = 'block';
+        for(let i=0;i<len;i++){
+            if(Number(idno)!=i){
+                console.log(Number(idno),i)
+                document.getElementById(`data_a${i}`).style.display = 'none';
+            }
+        }
+    }
+}
+window.onmouseout = e =>{
+    var id = e.target.id;
+    if(id.substring(0,8) != 'data_div'){
+        for(let i=0;i<len;i++){
+                document.getElementById(`data_a${i}`).style.display = 'none';
+        }
+    }
+}
